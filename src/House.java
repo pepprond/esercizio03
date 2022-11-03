0f361891eb9571ca0910c334a712ad6df3c1ac82
pepprond <rondelli95@gmail.com>
2022-11-03 14:26:16 +0100

    final commit

---
+++ src/House.java
@@ -0,0 +1,11 @@
+public class House {
+    public String address;
+    public int numberOfFloors;
+
+    public House(String address, int numberOfFloors){
+        this.address= address;
+        this.numberOfFloors=numberOfFloors;
+        System.out.println("Object house created");
+
+    }
+}
---
+++ src/Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+    public static void main(String[] args) {
+        House house1 = new House("via soldato d'alessandro", 14);
+        House house2 = new House("via onorevole bonfiglio", 12);
+    }
+}
